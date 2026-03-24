0fc45ae260f51b5bca49aea63db2e358c60480ec
Filip Witkowicz <filip.witkowicz@allegro.com>
2026-03-24 21:10:00 +0100

    adjust parameters to obl

--- iriw/iriw.sh
+++ iriw/iriw.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 #SBATCH -J iriw
-#SBATCH -p pmem
+#SBATCH -p obl
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -c 96
+#SBATCH -c 128
 #SBATCH --time=00:20:00
 #SBATCH --output=iriw_slurm_log.txt
 
 g++ -O3 -pthread iriw.cpp -o iriw
 
-echo "=== NUMA topology ==="
-lscpu --parse=CPU,CORE,SOCKET | grep -v '#' | awk -F, '{print "Thread "$1" -> Socket "$3}' | head -10
-echo "..."
-lscpu --parse=CPU,CORE,SOCKET | grep -v '#' | awk -F, '{print "Thread "$1" -> Socket "$3}' | tail -10
+echo "=== HOST ==="
+hostname
 
-time ./iriw 48 10000000 48 >> iriw_result.csv
+echo "=== CPU INFO ==="
+lscpu | grep -E "Model name|Socket|Core|Thread|CPU\(s\):|NUMA"
+
+echo "=== FULL TOPOLOGY ==="
+lscpu --parse=CPU,CORE,SOCKET | grep -v '#'
+
+
+time ./iriw 128 10000000 64 >> iriw_result.csv
